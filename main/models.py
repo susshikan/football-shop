c9ac470ac97ddd319c6b18d372708a155a6f1e6e
susshikan <muhamadh123g@gmail.com>
2025-09-17 08:27:34 +0700

    TUGAS3: Change int to uuid

--- main/models.py
+++ main/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
         ('historical_matches', 'Historical Matches')
     ]
 
-    id = models.BigAutoField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
